6b4bf5ce2d0bf92706d9e5108da3542775ca04a8
전은형 <ooooo5h91@gmail.com>
2022-01-16 18:18:07 +0900

    JSON 응답 내려주는 구조 설정
    api폴더의 파일들이 실제 로직을 담당하게끔 작성

--- server/app.py
+++ server/app.py
@@ -1,10 +1,15 @@
 # 플라스크 자체를 로딩
 from flask import Flask
 from server.db_connector import DBConnector
+from .api.user import test
 
 db = DBConnector()
 
 def create_app():
     app = Flask(__name__)
+    
+    @app.get("/test")
+    def api_test():
+        return test()
     
     return app
