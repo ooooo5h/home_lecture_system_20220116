6b12371d31fac111b8f8a1ca48c429d25e7e99b6
전은형 <ooooo5h91@gmail.com>
2022-01-16 20:54:47 +0900

    파라미터와 DB내용 검증하는 로직 추가
    모든 검증을 통과해야만 리뷰 작성이 가능

--- server/api/lecture/review.py
+++ server/api/lecture/review.py
@@ -1,6 +1,37 @@
 from server import db
 
 def write_review(params):
+    
+    score = float(params['score'])
+    
+    if not (1<=score<=5):
+        return{
+            'code' : 400,
+            'message' : '평점은 1 ~ 5 사이로 입력해라'
+        }, 400
+    
+    if len(params['title']) < 5:
+        return{
+            'code' : 400,
+            'meesage' : '제목 최소 5자 이상'
+        }, 400
+    
+    if len(params['content']) < 10:
+        return{
+        'code' : 400,
+        'meesage' : '내용 최소 10자 이상'
+    }, 400
+    
+    sql = f"SELECT * FROM lecture_user WHERE user_id = {params['user_id']} AND lecture_id = {params['lecture_id']}"
+    
+    query_result = db.executeOne(sql)
+    if query_result is None:
+        return{
+            'code' : 400,
+            'message' : '수강 해야지만 리뷰 작성 가능'
+        }, 400
+    
     return{
-        'code' : '임시 리뷰 작성'
+        'code' : 200,
+        'message' : '임시 리뷰작성 완료'
     }
