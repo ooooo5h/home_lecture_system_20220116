8b8284e409ce015fac051e105d3b7f12b1c907e9
전은형 <ooooo5h91@gmail.com>
2022-01-16 22:18:31 +0900

    모든 게시글을 최신순으로 조회
    게시글을 표현하는 Posts모델 클래스 추가

--- server/api/post/post.py
+++ server/api/post/post.py
@@ -1,8 +1,20 @@
 from server import db
+from server.model import Posts
 
 def get_all_posts(params):
+    
+    sql = f"SELECT * FROM posts ORDER BY created_at DESC"
+    
+    post_data_list = db.executeAll(sql)
+    
+    posts = [Posts(row).get_data_object() for row in post_data_list]
+    
     return{
-        '임시' : '모든 게시글 조회'
+        'code' : 200,
+        'message' : '모든 게시글 가져오기',
+        'data' : {
+            'posts' : posts,
+        }
     }
     
 # 특정 게시글 상세 조회 -/post/5
