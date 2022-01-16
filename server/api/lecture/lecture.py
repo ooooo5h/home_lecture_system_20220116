0266f9d51228a459016c96713ab5b1e187a85887
전은형 <ooooo5h91@gmail.com>
2022-01-16 20:07:48 +0900

    실제 강의 목록 전부 조회하는 기능 추가

--- server/api/lecture/lecture.py
+++ server/api/lecture/lecture.py
@@ -2,6 +2,17 @@ from server.model import Lectures
 from server import db
 
 def get_all_lectures(params):
+    
+    sql = f"SELECT * FROM lectures ORDER BY name"
+    
+    db_list = db.executeAll(sql)
+    
+    lectures = [Lectures(row).get_data_object() for row in db_list]
+        
     return {
-        'code' : '강의 목록 내려오기 테스트중'
+        'code' : 200,
+        'message' : '수강 목록 성공',
+        'data' : {
+            'lectures' : lectures,
+        }
     }
